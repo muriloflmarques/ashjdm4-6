a9f9d5945d559716b43eff22c57b1b294e2d078c
Murilo Lobato <muilo_lobato@outlook.com>
2020-07-30 14:53:34 -0300

    Infra.Data - Including ITmsContext

---
+++ Tms.Infra.Data/Interface/ITmsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tms.Infra.Data.Interface
+{
+    public interface ITmsContext
+    {
+        public DbSet<Domain.Task> Tasks { get; set; }
+
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
