e74d575a6bd2a9180c4d31a8ed0362936129beaa
Murilo Lobato <muilo_lobato@outlook.com>
2020-07-30 13:52:48 -0300

    Infra.CrossCutting - Creating TaskDto, EnumHelper & TaskStateEnum

---
+++ Tms.Infra.CrossCutting/DTOs/TaskDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tms.Infra.CrossCutting.DTOs
+{
+public   class TaskDto
+    {
+        public TaskDto ParentTask { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+        public int TaskState { get; set; }
+        public string TaskStateText { get; set; }
+        public TaskDto[] SubTasks { get; set; }
+    }
+}
---
+++ Tms.Infra.CrossCutting/Enums/TaskStateEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Tms.Infra.CrossCutting.Enums
+{
+    public enum TaskStateEnum
+    {
+        [EnumValueAsText("Planned")]
+        Planned = 0,
+
+        [EnumValueAsText("In Progress")]
+        InProgress = 5,
+
+        [EnumValueAsText("Completed")]
+        Completed = 10
+    }
+}
