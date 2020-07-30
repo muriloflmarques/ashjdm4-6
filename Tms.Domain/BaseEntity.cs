fe1d400d1f007793e2b61edab8d9a4540213599f
Murilo Lobato <muilo_lobato@outlook.com>
2020-07-30 13:07:07 -0300

    Domain - creating BaseEntity.cs and Task.cs

---
+++ Tms.Domain/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tms.Domain
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            this.CreationDate = DateTime.Now;
+            this.ChangeDate = null;
+            this.DeleteDate = null;
+        }
+
+        public void SetChangeDate() => this.ChangeDate = DateTime.Now;
+
+        public void SetDeleteDate() => this.DeleteDate = DateTime.Now;
+
+        public string Id { get; set; }
+
+        public DateTime CreationDate { get; private set; }
+        public DateTime? ChangeDate { get; private set; }
+        public DateTime? DeleteDate { get; private set; }
+    }
+}
---
+++ Tms.Domain/Task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Tms.Domain
+{
+    public class Task : BaseEntity
+    {
+        public Task(string name, string description, DateTime? startDate, DateTime? finishDate)
+            : base()
+        {
+            this.Name = name;
+            this.Description = description;
+            this.StartDate = startDate;
+            this.FinishDate = finishDate;
+        }
+
+        public Task(string name, string description)
+         : this(name, description, null, null) { }
+
+        private string _name;
+        private string _description;
+        private DateTime? _startDate;
+        private DateTime? _finishDate;
+        //State
+
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            private set { _description = value; }
+        }
+
+        public DateTime? StartDate
+        {
+            get { return _startDate; }
+            private set { _startDate = value; }
+        }
+
+        public DateTime? FinishDate
+        {
+            get { return _finishDate; }
+            private set { _finishDate = value; }
+        }
+
+        //State
+    }
+}
